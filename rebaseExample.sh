ad5a1b83ce7005015a544cf93c0bbd7f50178bd8
maxwolf621 <68631186+maxwolf621@users.noreply.github.com>
2021-08-01 21:56:43 +0800

    Update rebaseExample.sh

--- rebaseExample.sh
+++ rebaseExample.sh
@@ -5,11 +5,12 @@
 # ! [rejected]        main -> main (fetch first)
 # error: Fehler beim Versenden einiger Referenzen nach 'https://github.com/maxwolf621/OauthUser'
 
-#* To handel the problem
-git fetch origin master
-git merge origin master
+#---To handle Such Problem---
 
-#* if receiv new erro : non-fast-forward then
+git fetch origin main
+git merge origin main
+
+#IF receiv new erro : non-fast-forward then
 git fetch origin master:tmp
 
 # display like the following
@@ -22,12 +23,14 @@ git fetch origin master:tmp
 # * [neuer Branch]    main       -> tmp
 #   3d29b28..5c81650  main       -> origin/main
 
+#-------- REBASE -----------
+
 git rebase tmp
 # display like the following
 # Zun\u00e4chst wird der Branch zur\u00fcckgespult, um Ihre \u00c4nderungen darauf neu anzuwenden...
 # Wende an: remove file mvn/wrapper
 
-#* Point to main
+# push local(origin) to remote 
 git push origin HEAD:main
 
 #* Delete our tmporary branch
